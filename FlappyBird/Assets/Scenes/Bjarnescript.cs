b721745bbcd1df29f1e9891fce11283c4b7e8c27
ThibeVDM <thibe.vandemoortele@ll.sintgregorius.be>
2025-09-26 23:24:51 +0200

    .

--- FlappyBird/Assets/Scenes/Bjarnescript.cs
+++ FlappyBird/Assets/Scenes/Bjarnescript.cs
@@ -5,18 +5,27 @@ public class Bjarnescript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool bjarneIsAlive = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && bjarneIsAlive)
         {
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        bjarneIsAlive = false;
     }
 }
